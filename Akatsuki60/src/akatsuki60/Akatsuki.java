ded8bacb0b70ac389cce46069e358e721c1b66b3
SALMA NAFISA QURROTU'AINI <145917431+SalmaNafisa@users.noreply.github.com>
2023-11-30 12:13:39 +0700

    Add files via upload

---
+++ Akatsuki60/src/akatsuki60/Akatsuki.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package akatsuki60;
+
+/**
+ *
+ * @author ASUS TUF
+ * Nama : Salma Nafisa Qurrotu'Aini
+ * Nim : 22205038
+ * Kelas : Teknik Informatika
+ * Matakuliah : PBO1
+ * Semester : 3
+ * 
+ */
+public class Akatsuki {
+    protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+
+}
